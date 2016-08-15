4fb54eb2ee90fa7f6ffdfdfad3b74d4a3d349cc5
Florian <florian.prive.21@gmail.com>
2016-08-15 16:43:28 +0200

    base

---
+++ FormatPost.R
@@ -0,0 +1,64 @@
+FormatPost <- function(convert_file, githubrepo = NULL) {
+  # convert_file: name/path to specific file (any extension) to convert
+  # githubrepo: string giving your repository name (ex: "privefl") 
+  #             in order to have a link to the pure html document
+  
+  # without extension
+  prefix <- tools::file_path_sans_ext(convert_file)
+  # all 3 extensions
+  rmd <- paste0(prefix, ".Rmd")
+  md <- paste0(prefix, ".md")
+  html <- paste0(prefix, ".html")
+  
+  # write ref to html from github
+  if (is.null(githubrepo)) {
+    ref.to.html <- "\n\n***\n\n"
+  } else {
+    ref.to.html <-  paste0("\n\n***\n\n", 
+                           "<div style=\"text-align:center\">\n",
+                           "<a target=\"_blank\" ",
+                           "href=\"https://htmlpreview.github.io/?",
+                           "https://github.com/",
+                           githubrepo,
+                           "/blog/blob/gh-pages/",
+                           html,
+                           "\">View this as a standalone HTML page</a>\n",
+                           "</div>",
+                           "\n\n***\n\n")
+  }
+  
+  # read files
+  lines.rmd <- readLines(rmd, encoding = "UTF-8")
+  lines.md <- readLines(md, encoding = "UTF-8")
+  
+  # get yaml header (delimited by the first '---')
+  header.rmd <- grep("---", x = lines.rmd, fixed = TRUE)[2]
+  first.line <- header.rmd + 1
+  while (lines.rmd[first.line] == "") first.line <- first.line + 1
+  first.line.md <- grep(lines.rmd[first.line], lines.md, fixed = TRUE)
+  lines.md[1:(first.line.md - 1)] <- ""
+  
+  # get 'title', 'date' and 'layout' headers
+  title <- grep("title:", lines.rmd, fixed = TRUE)
+  date <- grep("date:", lines.rmd, fixed = TRUE)
+  lines.md[1] <- paste0(paste("---", lines.rmd[title], lines.rmd[date], 
+                              "layout: post", "---", "", sep = "\n"),
+                        ref.to.html)
+  
+  # get the right name output format
+  line.title <- sub("title:", "", lines.rmd[title])
+  line.title <- gsub('\"', "", line.title, fixed = TRUE)
+  suffix <- gsub("[ ]{1,}", "-", line.title)
+  md.path <- file.path("_posts", paste0(Sys.Date(), suffix, ".md"))
+  
+  # form MathJax syntax
+  lines.md <- gsub(pattern = "$", replacement = "$$", lines.md, fixed = TRUE)
+  lines.md <- gsub(pattern = "$$$$", replacement = "\n\n$$\n\n", 
+                   lines.md, fixed = TRUE)
+  
+  # replace file with new lines
+  writeLines(lines.md, md.path, useBytes = TRUE)
+  
+  return(md.path)
+}
+
---
+++ _posts/2016-08-15-A-minimal-R-Markdown-example.md
@@ -0,0 +1,63 @@
+---
+title: A minimal R Markdown example
+date: August 15, 2016
+layout: post
+---
+
+
+***
+
+
+
+
+## Quotes
+
+> I love R and I like writing articles with R Markdown.
+
+# Lists
+
+This is a bullet list:
+
+- item 1,
+- item 2.
+
+This is an ordered list:
+
+1. item 3,
+2. item 4.
+
+## Equations
+
+This is an inline equation with an inline R code chunk: $$\pi = 3.1415927$$.
+
+This is the Gaussian integral equation: 
+
+$$
+
+\int_{-\infty}^{+\infty} e^{-x^2}~dx = \sqrt{\pi}.
+
+$$
+
+
+
+## Plots
+
+
+```r
+knitr::opts_chunk$$set(fig.width=7, fig.height=5, fig.path = "../knitr_files/")
+```
+
+
+
+```r
+curve(exp(-x^2), -2.5, 2.5)
+```
+
+![](../knitr_files/unnamed-chunk-2-1.png)<!-- -->
+
+## Images
+
+<div style="text-align:center">    
+  ![Thanks GitHub for hosting my website!](http://moduslaborandi.net/wp-content/uploads/2015/05/github.pages.jpg)
+</div>
+
