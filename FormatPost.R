e91729a07bd64980cabf2f2a61bbc4ef204bbf20
Florian <florian.prive.21@gmail.com>
2016-08-17 22:08:20 +0200

    update FormatPost.R

--- FormatPost.R
+++ FormatPost.R
@@ -1,109 +1,82 @@
-FormatPost <- function(convert_file, githubrepo = NULL) {
-  # convert_file: name/path to specific file (any extension) to convert
-  # githubrepo: string giving your repository name (ex: "privefl") 
-  #             in order to have a link to the pure html document
+FormatPost <- function(rmd, knitr_files_dir = "knitr_files", 
+                       tmp_dir = "_built") {
+  # rmd: name/path to specific Rmd file to convert
+  # knitr_files_dir: The permanent directory where the files like plots
+  #                  are put
+  # tmp_dir: temporary directory for the temporary html file
   
-  # without extension
-  prefix <- tools::file_path_sans_ext(convert_file)
-  # all 3 extensions
-  rmd <- paste0(prefix, ".Rmd")
-  md <- paste0(prefix, ".md")
-  html <- paste0(prefix, ".html")
+  # verif
+  if (tools::file_ext(rmd) != "Rmd") stop("Input shoud be an R Markdown file")
   
-  # write ref to html from github
-  if (is.null(githubrepo)) {
-    ref.to.html <- "\n\n***\n\n"
-  } else {
-    ref.to.html <-  paste0("\n\n***\n\n", 
-                           "<div style=\"text-align:center\">\n",
-                           "<a target=\"_blank\" ",
-                           "href=\"https://htmlpreview.github.io/?",
-                           "https://github.com/",
-                           githubrepo,
-                           "/blog/blob/gh-pages/",
-                           html,
-                           "\">View this as a standalone HTML page</a>\n",
-                           "</div>",
-                           "\n\n***\n\n")
-  }
+  # knit
+  html <- rmarkdown::render(rmd, 
+                            prettydoc::html_pretty(highlight = "github", 
+                                                   self_contained = FALSE),
+                            output_dir = tmp_dir, 
+                            clean = FALSE)
   
-  # read files
+  # Read rmd
   lines.rmd <- readLines(rmd, encoding = "UTF-8")
-  lines.md <- readLines(md, encoding = "UTF-8")
+  lines.html <- readLines(html, encoding = "UTF-8")
+  
+  # get main section
+  section.begin <- grep("<section class=\"main-content\">", 
+                        lines.html, fixed = TRUE)
+  section.ends <- grep("</section>", lines.html, fixed = TRUE)
+  section.end <- min(section.ends[section.ends > section.begin])
+  lines.html <- lines.html[section.begin:section.end]
   
   # get yaml header (delimited by the first '---')
   header.rmd <- grep("---", x = lines.rmd, fixed = TRUE)[2]
-  first.line <- header.rmd + 1
-  while (lines.rmd[first.line] == "") first.line <- first.line + 1
-  first.line.md <- grep(lines.rmd[first.line], lines.md, fixed = TRUE)
-  lines.md[1:(first.line.md - 1)] <- ""
+  if (is.na(header.rmd)) stop("Your document should have an YAML header")
   
   # get 'title', 'date' and 'layout' headers
-  title <- grep("title:", lines.rmd, fixed = TRUE)
-  date <- grep("date:", lines.rmd, fixed = TRUE)
-  lines.md[1] <- paste0(paste("---", lines.rmd[title], lines.rmd[date], 
-                              "layout: post", "---", "", sep = "\n"),
-                        ref.to.html)
+  title <- grep("title:", lines.rmd[1:header.rmd], fixed = TRUE)
+  if (length(title) == 0) stop("Your document should have an YAML title")
+  date <- grep("date:", lines.rmd[1:header.rmd], fixed = TRUE)
+  if (length(date) == 0) stop("Your document should have an YAML date")
+  new.header <- paste("---", lines.rmd[title], lines.rmd[date],
+                      "layout: post", "---", "", sep = "\n")
   
   # get the right name output format
-  line.title <- sub("title:", "", lines.rmd[title])
+  line.title <- sub("title:", "", lines.rmd[title], fixed = TRUE)
   line.title <- gsub('\"', "", line.title, fixed = TRUE)
   suffix <- gsub("[ ]{1,}", "-", line.title)
   md.path <- file.path("_posts", paste0(Sys.Date(), suffix, ".md"))
   
-  # form MathJax syntax
-  lines.md <- gsub(pattern = "$", replacement = "$$", lines.md, fixed = TRUE)
-  lines.md <- gsub(pattern = "$$$$", replacement = "\n\n$$\n\n", 
-                   lines.md, fixed = TRUE)
-  
   # Change path to figures
-  base.prefix <- basename(prefix)
-  base.dir <- dirname(prefix)
-  pattern <-  paste0(base.prefix, "_files")
-  tmp <- gsubfn::strapply(lines.md, pattern)
+  rmd.base <- sub("\\.Rmd$", "", basename(rmd))
+  pattern <-  paste0(rmd.base, "_files")
+  tmp <- gsubfn::strapply(lines.html, pattern)
   ind <- which(!sapply(tmp, is.null))
   for (i in ind) {
-    lines.md[i] <-
-      gsubfn::gsubfn(pattern, file.path("{{ site.baseurl }}", "knitr_files",
-                                        tmp[[i]][1]), lines.md[i])
+    lines.html[i] <-
+      gsubfn::gsubfn(pattern, file.path("{{ site.baseurl }}", knitr_files_dir,
+                                        tmp[[i]][1]), lines.html[i])
   }
-  dir.source <- file.path(base.dir, pattern)
-  dir.target <- "knitr_files"
-  if (!dir.exists(dir.target)) dir.create(dir.target)
-  file.copy(from = dir.source, to = dir.target, 
+  dir.source <- file.path(tmp_dir, pattern)
+  if (!dir.exists(knitr_files_dir)) dir.create(knitr_files_dir)
+  file.copy(from = dir.source, to = knitr_files_dir, 
             overwrite = TRUE, recursive = TRUE)
   
   # Change path to images
-  pattern <- file.path("\\.\\.", "images")
-  tmp <- gsubfn::strapply(lines.md, pattern)
+  pattern <- getwd()
+  tmp <- gsubfn::strapply(lines.html, pattern)
   ind <- which(!sapply(tmp, is.null))
   for (i in ind) {
-    lines.md[i] <-
-      gsubfn::gsubfn(pattern, file.path("{{ site.baseurl }}", "images"), lines.md[i])
-  }
-  
-  # Center images and add proper caption
-  tmp <- gsubfn::strapply(lines.md, "!\\[[^]]*\\]\\([^)]+\\)")
-  ind <- which(!sapply(tmp, is.null))
-  for (i in ind) {
-    for (k in 1:length(tmp[[i]])) {
-      img.text <- gsubfn::strapply(tmp[[i]][k], "!\\[(.*)\\]\\(.+\\)")[[1]]
-      img.url <- gsubfn::strapply(tmp[[i]][k], "!\\[.*\\]\\((.+)\\)")[[1]]
-      caption <- ifelse(img.text == "", "", 
-                        paste0("<p class=\"caption\">", img.text, "</p>"))
-      lines.md[i] <- 
-        gsubfn::gsubfn(sprintf("!\\[%s\\]\\(%s\\)", img.text, img.url), 
-                       paste("<div class=\"figure\" style=\"text-align: center\">",
-                             paste0("<img src=\"", img.url, 
-                                    "\" alt=\"", img.text, "\"  />"),
-                             caption,
-                             "</div>", sep = "\n"), lines.md[i])
-    }
+    lines.html[i] <-
+      gsubfn::gsubfn(pattern, "{{ site.baseurl }}", lines.html[i])
   }
   
   # create file with new lines
   if (!dir.exists("_posts")) dir.create("_posts")
-  writeLines(lines.md, md.path, useBytes = TRUE)
+  writeLines(c(new.header, "", "", lines.html),  
+             md.path, useBytes = TRUE)
+  
+  # remove temporary directory and clean build files
+  unlink(tmp_dir, recursive=TRUE)
+  file.remove(sub(pattern = "\\.Rmd$", ".knit.md", rmd))
+  file.remove(sub(pattern = "\\.Rmd$", ".utf8.md", rmd))
   
   return(md.path)
 }
